cb3447437f04ac7d38665ec7f4e960df630f7d45
beomrae <whqjafo111@baver.com>
2023-08-14 14:59:58 +0900

    814 15시

--- lib/my/myfeed.dart
+++ lib/my/myfeed.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class MyFeedPage extends StatefulWidget {
   const MyFeedPage({Key? key}) : super(key: key);
@@ -8,14 +10,61 @@ class MyFeedPage extends StatefulWidget {
 }
 
 class _MyFeedPageState extends State<MyFeedPage> {
+  User? user;
+  Map<String, dynamic>? userInfo;
+
+  @override
+  void initState() {
+    super.initState();
+    user = FirebaseAuth.instance.currentUser;
+    if (user != null) {
+      fetchUserInfo(user!);
+    }
+  }
+
+  Future<void> fetchUserInfo(User user) async {
+    DocumentSnapshot doc = await FirebaseFirestore.instance
+        .collection('users')
+        .doc(user.uid)
+        .get();
+    setState(() {
+      userInfo = doc.data() as Map<String, dynamic>?;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('내 피드'),
-      ),
-      body: const Center(
-        child: Text('내 피드 내용이 여기에 표시됩니다.'),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Row(
+                children: [
+                  IconButton(
+                    icon: const Icon(
+                      Icons.arrow_back, // 뒤로가기 버튼 아이콘
+                      color: Colors.black, // 아이콘의 색상을 검은색으로 설정
+                    ),
+                    onPressed: () => Navigator.pop(context), // 뒤로가기 기능
+                  ),
+                  if (userInfo != null) ...[
+                    const Text(
+                      "My Feed", // 로그인한 사람의 닉네임 표시
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ],
+              ),
+              // 나머지 부분의 UI와 기능은 여기에 추가하십시오.
+            ],
+          ),
+        ),
       ),
     );
   }

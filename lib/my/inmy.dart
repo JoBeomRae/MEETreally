1175fce9645c9dbc16b25c4131214129e1ce5025
beomrae <whqjafo111@baver.com>
2023-08-28 10:18:24 +0900

    828 10:18

--- lib/my/inmy.dart
+++ lib/my/inmy.dart
@@ -74,138 +74,189 @@ class _InMyState extends State<InMy> {
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 255, 255, 255),
       body: Padding(
-        padding: const EdgeInsets.only(top: 80.0, left: 20.0),
+        padding: const EdgeInsets.symmetric(horizontal: 16.0),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.center, // 추가된 줄
+          crossAxisAlignment: CrossAxisAlignment.center, // 변경된 줄
+
           children: [
-            const Text(
-              '마이페이지',
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-                color: Colors.black,
-              ),
-            ),
+            const MyPageTitle(),
             const SizedBox(height: 50),
-            Padding(
-              padding: const EdgeInsets.only(top: 10.0, left: 60, bottom: 50.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Stack(
-                    alignment: Alignment.bottomRight,
-                    children: [
-                      Container(
-                        padding: const EdgeInsets.all(5.0),
-                        decoration: const BoxDecoration(
-                          color: Color(0xFFE06292),
-                          shape: BoxShape.circle,
-                        ),
-                        child: CircleAvatar(
-                          radius: 88,
-                          backgroundImage: _imageURL != null
-                              ? NetworkImage(_imageURL!)
-                              : null,
-                          backgroundColor: const Color.fromARGB(255, 0, 0, 0),
-                        ),
-                      ),
-                      Positioned(
-                        bottom: 0,
-                        right: 0,
-                        child: GestureDetector(
-                          onTap: _pickImage,
-                          child: Container(
-                            width: 30,
-                            height: 30,
-                            decoration: const BoxDecoration(
-                              color: Color(0xFFE06292),
-                              shape: BoxShape.circle,
-                            ),
-                            child: const Icon(
-                              Icons.add,
-                              size: 30,
-                              color: Color.fromARGB(255, 255, 255, 255),
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(height: 20),
-                  Container(
-                    padding: const EdgeInsets.only(left: 40.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: userInfo != null
-                          ? [
-                              Text("${userInfo!['nickname']}",
-                                  style: const TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 40)),
-                              Text(
-                                  "${userInfo!['name']} (${userInfo!['age'].toString()}) ${userInfo!['job']}"),
-                            ]
-                          : [
-                              const Text("정보를 가져오는 중..."),
-                            ],
-                    ),
-                  ),
-                  const SizedBox(height: 20),
-                  Container(
-                    padding: const EdgeInsets.only(left: 30.0),
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: const Color.fromARGB(255, 0, 0, 0),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                      ),
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => MyFeedPage(
-                              nickname: userInfo!['nickname'],
-                            ),
-                          ),
-                        );
-                      },
-                      child: const Text(
-                        '내 피드 보러가기',
-                        style: TextStyle(color: Colors.white),
-                      ),
-                    ),
-                  ),
-                  const SizedBox(height: 5),
-                  Container(
-                    padding: const EdgeInsets.only(left: 40.0),
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: const Color.fromARGB(255, 0, 0, 0),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                      ),
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => const FriendList(),
-                          ),
-                        );
-                      },
-                      child: const Text(
-                        '친구목록 보기',
-                        style: TextStyle(color: Colors.white),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
+            ProfileAndUserInfo(
+              imageURL: _imageURL,
+              userInfo: userInfo,
+              pickImage: _pickImage,
             ),
+            const SizedBox(height: 20),
+            ActionButtons(userInfo: userInfo),
           ],
         ),
       ),
+    );
+  }
+}
+
+class MyPageTitle extends StatelessWidget {
+  const MyPageTitle({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Padding(
+      padding: EdgeInsets.only(),
+      child: Text(
+        '마이페이지',
+        style: TextStyle(
+          fontSize: 24,
+          fontWeight: FontWeight.bold,
+          color: Colors.black,
+        ),
+      ),
+    );
+  }
+}
+
+class ProfileAndUserInfo extends StatelessWidget {
+  final String? imageURL;
+  final Map<String, dynamic>? userInfo;
+  final Function()? pickImage;
+
+  const ProfileAndUserInfo(
+      {Key? key, this.imageURL, this.userInfo, this.pickImage})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: Padding(
+      padding: const EdgeInsets.only(),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Stack(
+            alignment: Alignment.bottomRight,
+            children: [
+              Container(
+                padding: const EdgeInsets.all(5.0),
+                decoration: const BoxDecoration(
+                  color: Color(0xFFE06292),
+                  shape: BoxShape.circle,
+                ),
+                child: CircleAvatar(
+                  radius: 88,
+                  backgroundImage:
+                      imageURL != null ? NetworkImage(imageURL!) : null,
+                  backgroundColor: imageURL != null
+                      ? null
+                      : const Color.fromARGB(255, 0, 0, 0),
+                ),
+              ),
+              Positioned(
+                bottom: 0,
+                right: 0,
+                child: GestureDetector(
+                  onTap: () => pickImage!(),
+                  child: Container(
+                    width: 30,
+                    height: 30,
+                    decoration: const BoxDecoration(
+                      color: Color(0xFFE06292),
+                      shape: BoxShape.circle,
+                    ),
+                    child: const Icon(
+                      Icons.add,
+                      size: 30,
+                      color: Color.fromARGB(255, 255, 255, 255),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 20),
+          Container(
+            padding: const EdgeInsets.only(),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: userInfo != null
+                  ? [
+                      Text("${userInfo!['nickname']}",
+                          style: const TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 40)),
+                      Text(
+                          "${userInfo!['name']} (${userInfo!['age'].toString()}) ${userInfo!['job']}"),
+                    ]
+                  : [
+                      const Text("정보를 가져오는 중..."),
+                    ],
+            ),
+          ),
+        ],
+      ),
+    ));
+  }
+}
+
+class ActionButtons extends StatelessWidget {
+  final Map<String, dynamic>? userInfo;
+
+  const ActionButtons({Key? key, this.userInfo}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          width: double.infinity,
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: const Color.fromARGB(255, 0, 0, 0),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20),
+              ),
+            ),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => MyFeedPage(
+                    nickname: userInfo!['nickname'],
+                  ),
+                ),
+              );
+            },
+            child: const Text(
+              '내 피드 보러가기',
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+        ),
+        const SizedBox(height: 5),
+        Container(
+          width: double.infinity,
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: const Color.fromARGB(255, 0, 0, 0),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20),
+              ),
+            ),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const FriendList(),
+                ),
+              );
+            },
+            child: const Text(
+              '친구목록 보기',
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
